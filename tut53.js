622321da58b6c41e415a0561bdee200189208bbe
im-kamil <kamilnasar786@gmail.com>
2022-05-15 17:31:01 +0530

    aiwehi

---
+++ tut53.js
@@ -0,0 +1,13 @@
+console.log("javascript function");
+
+let name = "kamil";
+let name1 = "kamil";
+let name2 = "kamil";
+let name3 = "kamil";
+let name4 = "kamil";
+
+console.log( name + " is a bad boy");
+console.log( name1 + " is a bad boy");
+console.log( name2 + " is a bad boy");
+console.log( name3 + " is a bad boy");
+console.log( name4 + " is a bad boy");
