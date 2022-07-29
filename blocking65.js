57f771f05424a5abaccac198fb7946d0506ddb07
im-kamil <kamilnasar786@gmail.com>
2022-07-29 17:29:48 +0530

    server and blocking

--- blocking65.js
+++ blocking65.js
@@ -2,6 +2,6 @@
 // A synchronous or non-blocking - line by line execution not guaranteed , Callbacks will fire 
 const fs = require("fs");
 let  text = fs.readFile("farhan.html" , "utf-8", (error, data)=>{
-    console.log( data);
+    // console.log( data);
 });
 console.log("This is a message");
